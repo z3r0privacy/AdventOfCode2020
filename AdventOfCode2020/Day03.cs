4bfeb2aff30baf6e48d1c3b1ee9987db2ab43edd
Michael Roth <michi_roth@gmx.ch>
2020-12-04 07:34:56 +0100

    added days 3&4, prepared day5

---
+++ AdventOfCode2020/Day03.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace AdventOfCode2020
+{
+    class Day03 : AbstractDay
+    {
+        private protected override object CachedResult1 => 191;
+        private protected override object CachedResult2 => 1478615040;
+
+
+        private protected override object Task1()
+        {
+            var map = ReadInputAsLines().Select(s => s.ToCharArray()).ToList();
+            int x = 0, y = 0, cnt = 0;
+            while (y < map.Count)
+            {
+                if (map[y][x] == '#')
+                {
+                    cnt++;
+                }
+                y++;
+                x = (x + 3) % map[0].Count();
+            }
+            return cnt;
+        }
+
+        private protected override object Task2()
+        {
+            var map = ReadInputAsLines().Select(s => s.ToCharArray()).ToList();
+
+            var dirs = new[] { (1, 1), (3, 1), (5, 1), (7, 1), (1, 2) };
+            var count = new List<int>();
+
+            foreach ((var mx, var my) in dirs)
+            {
+                int x = 0, y = 0, cnt = 0;
+                while (y < map.Count)
+                {
+                    if (map[y][x] == '#')
+                    {
+                        cnt++;
+                    }
+                    y+=my;
+                    x = (x + mx) % map[0].Count();
+                }
+                count.Add(cnt);
+            }
+            return count.Aggregate((i1, i2) => i1 * i2);
+        }
+    }
+}
---
+++ AdventOfCode2020/Day04.cs
@@ -0,0 +1,98 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+
+namespace AdventOfCode2020
+{
+    class Day04 : AbstractDay
+    {
+        private protected override object CachedResult1 => 206;
+        private protected override object CachedResult2 => 123;
+
+        private protected override object Task1()
+        {
+            var lines = ReadInputAsLines();
+            var numValids = 0;
+            var currentPass = 0;
+            var fields = new[] { "byr", "iyr", "eyr", "hgt", "hcl", "ecl", "pid" };
+            
+            foreach (var l in lines)
+            {
+                if (string.IsNullOrWhiteSpace(l))
+                {
+                    if (currentPass >= 7)
+                    {
+                        numValids++;
+                    }
+                    currentPass = 0;
+                } else
+                {
+                    foreach (var word in fields)
+                    {
+                        if (l.Contains($"{word}:"))
+                        {
+                            currentPass++;
+                        }
+                    }
+                }
+            }
+            if (currentPass >= 7) {
+                numValids++;
+            }
+
+            return numValids;
+        }
+
+        private protected override object Task2()
+        {
+            var lines = ReadInputAsLines();
+            var numValids = 0;
+            var currentPass = 0;
+            var fields = new[] { "byr", "iyr", "eyr", "hgt", "hcl", "ecl", "pid" };
+            var validiers = new Predicate<string>[]
+            {
+                str => int.TryParse(str, out var n) && n >= 1920 && n <= 2002,
+                str => int.TryParse(str, out var n) && n >= 2010 && n <= 2020,
+                str => int.TryParse(str, out var n) && n >= 2020 && n <= 2030,
+                str => (str[^2..]=="cm" && int.TryParse(str[0..^2], out var n) && n >= 150 && n <= 193)
+                      || (str[^2..]=="in" && int.TryParse(str[0..^2], out var n1) && n1 >= 59 && n1 <= 76),
+                str => Regex.IsMatch(str, "#[0-9a-f]{6}"),
+                str => new []{"amb", "blu", "brn", "gry", "grn", "hzl", "oth"}.Contains(str),
+                str => str.Length == 9 && int.TryParse(str, out var _)
+            };
+
+            foreach (var l in lines)
+            {
+                if (string.IsNullOrWhiteSpace(l))
+                {
+                    if (currentPass >= 7)
+                    {
+                        numValids++;
+                    }
+                    currentPass = 0;
+                }
+                else
+                {
+                    var tkns = l.Tokenize(": ");
+                    for (var i = 0; i < fields.Length; i++)
+                    {
+                        var idx = tkns.IndexOf(fields[i]);
+                        if (idx != -1)
+                        {
+                            if (validiers[i](tkns[idx + 1])) {
+                                currentPass++;
+                            }
+                        }
+                    }
+                }
+            }
+            if (currentPass >= 7) {
+                numValids++;
+            }
+
+            return numValids;
+        }
+    }
+}
