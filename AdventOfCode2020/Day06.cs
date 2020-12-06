dc3fb996d6cd99621af2add15a2997471e11c1f2
Michael Roth <michi_roth@gmx.ch>
2020-12-06 17:07:47 +0100

    added day6, prepared day7

--- AdventOfCode2020/Day06.cs
+++ AdventOfCode2020/Day06.cs
@@ -1,19 +1,53 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Text;
 
 namespace AdventOfCode2020
 {
     class Day06 : AbstractDay
     {
+        private protected override object CachedResult1 => 6768;
+        private protected override object CachedResult2 => 3489;
+
         private protected override object Task1()
         {
-            throw new NotImplementedException();
+            var lines = ReadInputAsLines();
+            var sum = 0;
+            var str = "";
+            foreach (var l in lines)
+            {
+                if (string.IsNullOrWhiteSpace(l))
+                {
+                    sum += str.Distinct().Count();
+                    str = "";
+                } else
+                {
+                    str += l;
+                }
+            }
+            sum += str.Distinct().Count();
+            return sum;
         }
 
         private protected override object Task2()
         {
-            throw new NotImplementedException();
+            var lines = ReadInputAsLines();
+            var current = new List<string>();
+            var sum = 0;
+            foreach (var l in lines)
+            {
+                if (string.IsNullOrWhiteSpace(l))
+                {
+                    sum += Enumerable.Range('a', 26).Count(c => current.All(e => e.Contains((char)c)));
+                    current.Clear();
+                } else
+                {
+                    current.Add(l);
+                }
+            }
+            sum += Enumerable.Range('a', 26).Count(c => current.All(e => e.Contains((char)c)));
+            return sum;
         }
     }
 }
