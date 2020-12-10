76efd0efb5639b7c12c057cfabee224f7f8d377c
Michael Roth <michi_roth@gmx.ch>
2020-12-10 19:04:30 +0100

    added days 9, 10 - prepared day 11

--- AdventOfCode2020/Day09.cs
+++ AdventOfCode2020/Day09.cs
@@ -1,19 +1,64 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Text;
 
 namespace AdventOfCode2020
 {
     class Day09 : AbstractDay
     {
+        private protected override object CachedResult1 => 1038347917;
+        private protected override object CachedResult2 => 137394018;
+        internal override string DayName => "Encoding Error";
         private protected override object Task1()
         {
-            throw new NotImplementedException();
+            var nums = ReadInputAsLines<Int64>(Int64.TryParse);
+            bool canCreateNum(int i)
+            {
+                for (var j = i - 25; j < i; j++)
+                {
+                    for (var y = j + 1; y < i; y++)
+                    {
+                        if (nums[j] + nums[y] == nums[i])
+                        {
+                            return true;
+                        }
+                    }
+                }
+                return false;
+            }
+            for (var i = 25; i < nums.Length; i++)
+            {
+                if (!canCreateNum(i))
+                {
+                    return nums[i];
+                }
+            }
+            throw new Exception();
         }
 
         private protected override object Task2()
         {
-            throw new NotImplementedException();
+            var goal = 1038347917L;
+            var nums = ReadInputAsLines<Int64>(Int64.TryParse);
+            var start = 0;
+            while (true)
+            {
+                var sum = 0L;
+                var curr = start;
+                while (sum < goal)
+                {
+                    sum += nums[curr];
+                    curr++;
+                }
+                if (sum == goal && (curr-1) > start)
+                {
+                    var len = curr - start;
+                    var seq = nums.Skip(start).Take(len);
+                    return seq.Min() + seq.Max();
+                }
+                start++;
+            }
         }
     }
 }
---
+++ AdventOfCode2020/Day10.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace AdventOfCode2020
+{
+    class Day10 : AbstractDay
+    {
+        private protected override object CachedResult1 => 1700;
+        private protected override object CachedResult2 => 12401793332096;
+        internal override string DayName => "Adapter Array";
+        private protected override object Task1()
+        {
+            var nums = ReadInputAsLines<int>(int.TryParse).OrderBy(i => i);
+            var q = new Queue<int>(nums);
+            var currOut = 0;
+            var num1 = 0;
+            var num3 = 0;
+            while (q.Count > 0)
+            {
+                var next = q.Dequeue();
+                var diff = next - currOut;
+                if (diff > 3 || diff < 1)
+                {
+                    throw new Exception();
+                }
+                if (diff == 1) num1++;
+                if (diff == 3) num3++;
+                currOut = next;
+            }
+            num3++;
+            return num1 * num3;
+        }
+
+        private long FindWays(int curr, List<int> remaining, int goal)
+        {
+            if (curr == goal) return 1;
+            if (curr > goal) return 0;
+
+            var count = 0L;
+            foreach (var r in new List<int>(remaining))
+            {
+                var diff = r - curr;
+                if (diff > 0 && diff <= 3)
+                {
+                    remaining.Remove(r);
+                    count += FindWays(r, remaining, goal);
+                    remaining.Add(r);
+                }
+            }
+            return count;
+        }
+        private protected override object Task2()
+        {
+            var nums = ReadInputAsLines<int>(int.TryParse).OrderBy(i => i).ToList();
+
+            var possibilities = new List<long>();
+            var start = 0;
+            for (var i = 1; i < nums.Count; i++)
+            {
+                if (nums[i] - nums[i - 1] == 3)
+                {
+                    possibilities.Add(FindWays(start, nums.ToList(), nums[i - 1])); ;
+                    start = nums[i];
+                }
+            }
+            possibilities.Add(FindWays(start, nums.ToList(), nums.Last()));
+
+            return possibilities.Aggregate((l1,l2) => l1*l2);
+        }
+    }
+}
