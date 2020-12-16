875b63bdc2d9303c3d2e7fc9ba8364aa8c2b5544
Michael Roth <michi_roth@gmx.ch>
2020-12-16 10:50:26 +0100

    Performance improvement and more consistency in day 15

--- AdventOfCode2020/Day15.cs
+++ AdventOfCode2020/Day15.cs
@@ -7,51 +7,49 @@ namespace AdventOfCode2020
 {
     class Day15 : AbstractDay
     {
-        private protected override object CachedResult1 => 447;
-        private protected override object CachedResult2 => 11721679;
+        //private protected override object CachedResult1 => 447;
+        //private protected override object CachedResult2 => 11721679;
         internal override string DayName => "Rambunctious Recitation";
+
+        private List<int> _numbers;
+        private protected override void Init()
+        {
+            _numbers = ReadInput().Split(',').Select(str => int.Parse(str)).ToList();
+        }
+
+        private int MemoryGame(int goal)
+        {
+            var localNumbers = _numbers.ToList();
+            var lastSeen = new int[goal];  // ca 120MB, needs more RAM, but faster / use dictionary for a bit slower but smaller solution
+            // new Dictionary<int, int>();
+            for (var i = 0; i < localNumbers.Count - 1; i++)
+            {
+                lastSeen[localNumbers[i]] = i + 1;
+            }
+            var flow = localNumbers.Last();
+            while (localNumbers.Count < goal)
+            {
+                var nextNr = 0;
+//                if (lastSeen.ContainsKey(flow))
+                if (lastSeen[flow] != 0)
+                {
+                    nextNr = localNumbers.Count - lastSeen[flow];
+                }
+                lastSeen[flow] = localNumbers.Count;
+                localNumbers.Add(flow);
+                flow = nextNr;
+            }
+            return flow;
+        }
 
         private protected override object Task1()
         {
-            var numbers = ReadInput().Split(',').Select(str => int.Parse(str)).ToList();
-            while (numbers.Count < 2020)
-            {
-                var lastNr = numbers.Last();
-                var nextNr = 0;
-                for (var i = 1; i < numbers.Count; i++)
-                {
-                    if (numbers[numbers.Count-1-i] == lastNr)
-                    {
-                        nextNr = i;
-                        break;
-                    }
-                }
-                numbers.Add(nextNr);
-            }
-            return numbers.Last();
+            return MemoryGame(2020);
         }
 
         private protected override object Task2()
         {
-            var numbers = ReadInput().Split(',').Select(str => int.Parse(str)).ToList();
-            var lastSeen = new Dictionary<int, int>();
-            for (var i = 0; i < numbers.Count-1; i++)
-            {
-                lastSeen[numbers[i]] = i+1;
-            }
-            var flow = numbers.Last();
-            while (numbers.Count < 30000000)
-            {
-                var nextNr = 0;
-                if (lastSeen.ContainsKey(flow))
-                {
-                    nextNr = numbers.Count - lastSeen[flow];
-                }
-                lastSeen[flow] = numbers.Count;
-                numbers.Add(flow);
-                flow = nextNr;
-            }
-            return flow;
+            return MemoryGame(30000000);
         }
     }
 }
