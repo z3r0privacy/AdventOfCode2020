8ec494daa1db4d08a3d721af9204cac84e2842fa
Michael Roth <michi_roth@gmx.ch>
2020-12-16 07:45:46 +0100

    added days 15,16, prepared day17

--- AdventOfCode2020/Day15.cs
+++ AdventOfCode2020/Day15.cs
@@ -1,19 +1,57 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Text;
 
 namespace AdventOfCode2020
 {
     class Day15 : AbstractDay
     {
+        private protected override object CachedResult1 => 447;
+        private protected override object CachedResult2 => 11721679;
+        internal override string DayName => "Rambunctious Recitation";
+
         private protected override object Task1()
         {
-            throw new NotImplementedException();
+            var numbers = ReadInput().Split(',').Select(str => int.Parse(str)).ToList();
+            while (numbers.Count < 2020)
+            {
+                var lastNr = numbers.Last();
+                var nextNr = 0;
+                for (var i = 1; i < numbers.Count; i++)
+                {
+                    if (numbers[numbers.Count-1-i] == lastNr)
+                    {
+                        nextNr = i;
+                        break;
+                    }
+                }
+                numbers.Add(nextNr);
+            }
+            return numbers.Last();
         }
 
         private protected override object Task2()
         {
-            throw new NotImplementedException();
+            var numbers = ReadInput().Split(',').Select(str => int.Parse(str)).ToList();
+            var lastSeen = new Dictionary<int, int>();
+            for (var i = 0; i < numbers.Count-1; i++)
+            {
+                lastSeen[numbers[i]] = i+1;
+            }
+            var flow = numbers.Last();
+            while (numbers.Count < 30000000)
+            {
+                var nextNr = 0;
+                if (lastSeen.ContainsKey(flow))
+                {
+                    nextNr = numbers.Count - lastSeen[flow];
+                }
+                lastSeen[flow] = numbers.Count;
+                numbers.Add(flow);
+                flow = nextNr;
+            }
+            return flow;
         }
     }
 }
---
+++ AdventOfCode2020/Day16.cs
@@ -0,0 +1,108 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace AdventOfCode2020
+{
+    class Day16 : AbstractDay
+    {
+        private protected override object CachedResult1 => 27911;
+        private protected override object CachedResult2 => 737176602479;
+        internal override string DayName => "Ticket Translation";
+
+        private int[] _myTicket;
+        private List<int[]> _foreignTickets;
+        private List<(string name, Predicate<int> pred)> _rules;
+
+        private protected override void Init()
+        {
+            _foreignTickets = new List<int[]>();
+            _rules = new List<(string name, Predicate<int> pred)>();
+
+            var lines = ReadInputAsLines();
+            var i = 0;
+            for (; !string.IsNullOrWhiteSpace(lines[i]); i++)
+            {
+                var rule = lines[i].Split(":");
+                var predTkns = rule[1].Tokenize("- ");
+                _rules.Add((rule[0],
+                    num => (num >= int.Parse(predTkns[0]) && num <= int.Parse(predTkns[1]))
+                    || (num >= int.Parse(predTkns[3]) && num <= int.Parse(predTkns[4]))));
+            }
+
+            i += 2;
+            _myTicket = lines[i].Split(",").Select(str => int.Parse(str)).ToArray();
+
+            i += 3;
+            for (; i < lines.Length; i++)
+            {
+                _foreignTickets.Add(lines[i].Split(",").Select(str => int.Parse(str)).ToArray());
+            }
+        }
+
+        private protected override object Task1()
+        {
+            var sum = 0;
+            foreach (var ticket in _foreignTickets)
+            {
+                foreach (var value in ticket)
+                {
+                    if (_rules.All(r => !r.pred(value)))
+                    {
+                        sum += value;
+                    }
+                }
+            }
+            return sum;
+        }
+
+        private protected override object Task2()
+        {
+            var possibleLocs = new List<string>[_myTicket.Length];
+            for (var i = 0; i < possibleLocs.Length; i++) possibleLocs[i] = new List<string>();
+            var validTickets = new List<int[]>();
+            
+            foreach (var ticket in _foreignTickets)
+            {
+                if (ticket.All(value => _rules.Any(r => r.pred(value))))
+                {
+                    validTickets.Add(ticket);
+                }
+            }
+            validTickets.Add(_myTicket);
+
+            foreach ((var name, var rule) in _rules)
+            {
+                for (var i = 0; i < possibleLocs.Length; i++)
+                {
+                    if (validTickets.All(t => rule(t[i])))
+                    {
+                        possibleLocs[i].Add(name);
+                    }
+                }
+            }
+            var fixedNames = new List<string>();
+            while (possibleLocs.Any(l => l.Count > 1))
+            {
+                fixedNames = possibleLocs.Where(l => l.Count == 1).Select(l => l[0]).ToList();
+                for (var i = 0; i < possibleLocs.Length; i++)
+                {
+                    if (possibleLocs[i].Count > 1)
+                    {
+                        fixedNames.ForEach(n => possibleLocs[i].Remove(n));
+                    }
+                }
+            }
+            var prod = 1L;
+            for (var i = 0; i < possibleLocs.Length; i++)
+            {
+                if (possibleLocs[i][0].StartsWith("departure"))
+                {
+                    prod *= _myTicket[i];
+                }
+            }
+            return prod;
+        }
+    }
+}
