a9f0f8cc12ebbd7d083579f51f5e2abba6e9b022
Michael Roth <michi_roth@gmx.ch>
2020-12-22 21:25:46 +0100

    added day 22, prepared day 23

--- AdventOfCode2020/Day22.cs
+++ AdventOfCode2020/Day22.cs
@@ -1,19 +1,118 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Text;
 
 namespace AdventOfCode2020
 {
     class Day22 : AbstractDay
     {
+        private protected override object CachedResult1 => 32598;
+        private protected override object CachedResult2 => 35836;
+        internal override string DayName => "Crab Combat";
+        internal override bool IsLongRunning2 => true;
         private protected override object Task1()
         {
-            throw new NotImplementedException();
+            var input = ReadInputAsLines();
+            var player1 = new Queue<int>(input.Skip(1).TakeWhile(l => !string.IsNullOrWhiteSpace(l)).Select(l => int.Parse(l)));
+            var player2 = new Queue<int>(input.SkipWhile(l => !string.IsNullOrWhiteSpace(l)).Skip(2).Select(l => int.Parse(l)));
+
+            while (player1.Count > 0 && player2.Count > 0)
+            {
+                var card1 = player1.Dequeue();
+                var card2 = player2.Dequeue();
+                if (card1 > card2)
+                {
+                    player1.Enqueue(card1);
+                    player1.Enqueue(card2);
+                } else
+                {
+                    player2.Enqueue(card2);
+                    player2.Enqueue(card1);
+                }
+            }
+
+            var winningDeck = player1.Count == 0 ? player2 : player1;
+            var sum = 0L;
+            while (winningDeck.Count > 0)
+            {
+                sum += winningDeck.Count * winningDeck.Dequeue();
+            }
+            return sum;
+        }
+
+        private Dictionary<int, List<((Queue<int> p1In,Queue<int> p2In),int winner)>> _cache;
+
+        private int PlayGame(Queue<int> p1, Queue<int> p2)
+        {
+            var id = 100 * p1.Count + p2.Count;
+            if (_cache.TryGetValue(id, out var list))
+            {
+                foreach (((var q1, var q2), var w) in list)
+                {
+                    if (q1.SequenceEqual(p1) && q2.SequenceEqual(p2))
+                    {
+                        return w;
+                    }
+                }
+            } else
+            {
+                _cache.Add(id, new List<((Queue<int> p1In, Queue<int> p2In), int winner)>());
+            }
+            var input = (new Queue<int>(p1), new Queue<int>(p2));
+            var states = new List<(Queue<int> p1, Queue<int> p2)>();
+            while (p1.Count > 0 && p2.Count > 0)
+            {
+                foreach ((var sp1, var sp2) in states)
+                {
+                    if (sp1.SequenceEqual(p1) && sp2.SequenceEqual(p2))
+                    {
+                        _cache[id].Add((input, 1));
+                        return 1;
+                    }
+                }
+                states.Add((new Queue<int>(p1), new Queue<int>(p2)));
+                var c1 = p1.Dequeue();
+                var c2 = p2.Dequeue();
+                int winner;
+                if (p1.Count >= c1 && p2.Count >= c2)
+                {
+                    winner = PlayGame(new Queue<int>(p1.Take(c1)), new Queue<int>(p2.Take(c2)));
+                } else
+                {
+                    winner = c1 > c2 ? 1 : 2;
+                }
+                if (winner == 1)
+                {
+                    p1.Enqueue(c1);
+                    p1.Enqueue(c2);
+                } else
+                {
+                    p2.Enqueue(c2);
+                    p2.Enqueue(c1);
+                }
+            }
+            var win = p1.Count == 0 ? 2 : 1;
+            _cache[id].Add((input, win));
+            return win;
         }
 
         private protected override object Task2()
         {
-            throw new NotImplementedException();
+            _cache = new Dictionary<int, List<((Queue<int> p1In, Queue<int> p2In), int winner)>>();
+            var input = ReadInputAsLines();
+            var player1 = new Queue<int>(input.Skip(1).TakeWhile(l => !string.IsNullOrWhiteSpace(l)).Select(l => int.Parse(l)));
+            var player2 = new Queue<int>(input.SkipWhile(l => !string.IsNullOrWhiteSpace(l)).Skip(2).Select(l => int.Parse(l)));
+
+            var w = PlayGame(player1, player2);
+
+            var winningDeck = w == 1 ? player1 : player2;
+            var sum = 0L;
+            while (winningDeck.Count > 0)
+            {
+                sum += winningDeck.Count * winningDeck.Dequeue();
+            }
+            return sum;
         }
     }
 }
